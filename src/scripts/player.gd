497a93723a5d6206de96e9a08c8145bdd8692898
Lexal1 <86933786+Lexal1@users.noreply.github.com>
2026-02-19 16:42:22 -0700

    comments

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -7,10 +7,11 @@ func update_sprite(direction: Vector3) -> void:
 	#if direction.x != 0 and direction.y != 0 and direction.z != 0:
 	#print(direction)
 	# this code fails miserably if the player rotation isn't 0
-	#TODO: make not suck
+	#TODO: MAKE NOT SUCK!!!! This code will not work properly if the camera or the player rotation are different at all!
+	#      look into using an AnimationTree?
 	if direction.x > 0 and direction.z > 0:
 		$Sprite.play("walk_se")
-		return
+		return # i dont *care* if its better for readability, you do not need return spam in godot
 	if direction.x < 0 and direction.z < 0:
 		$Sprite.play("walk_nw")
 		return
@@ -47,7 +48,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# TODO: this script, while it works, sucks bad and needs to be rewritten
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -58,7 +59,6 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	update_sprite(direction)
 	move_and_slide()
-
 
 func _on_sprite_animation_finished() -> void:
 	$Sprite.stop()
