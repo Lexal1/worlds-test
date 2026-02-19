811bcd9f45e3689dcd64148fa9916ca3824be92e
Lexal1 <86933786+Lexal1@users.noreply.github.com>
2026-02-19 16:36:51 -0700

    add custom inputs
    this is just a good practice, or so ive been told

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 var SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 
@@ -8,6 +7,7 @@ func update_sprite(direction: Vector3) -> void:
 	#if direction.x != 0 and direction.y != 0 and direction.z != 0:
 	#print(direction)
 	# this code fails miserably if the player rotation isn't 0
+	#TODO: make not suck
 	if direction.x > 0 and direction.z > 0:
 		$Sprite.play("walk_se")
 		return
@@ -38,17 +38,17 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if Input.is_action_pressed("ui_cancel"):
+	if Input.is_action_pressed("button_2"):
 		SPEED = 6.0
 	else:
 		SPEED = 2.0
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("button_1") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
