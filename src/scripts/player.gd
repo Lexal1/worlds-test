936c9bdbd1bafd6f2e6f3eb7c71405a54eef5cfe
Lexal1 <86933786+Lexal1@users.noreply.github.com>
2026-02-19 16:53:16 -0700

    temp camera stuff and testing

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -6,9 +6,9 @@ const JUMP_VELOCITY = 4.5
 func update_sprite(direction: Vector3) -> void:
 	#if direction.x != 0 and direction.y != 0 and direction.z != 0:
 	#print(direction)
-	# this code fails miserably if the player rotation isn't 0
-	#TODO: MAKE NOT SUCK!!!! This code will not work properly if the camera or the player rotation are different at all!
+	#TODO: MAKE NOT SUCK!!!! This code will not work properly if the player rotation is different at all!
 	#      look into using an AnimationTree?
+	#      also looks horrific if you turn the camera because the sprite does not change
 	if direction.x > 0 and direction.z > 0:
 		$Sprite.play("walk_se")
 		return # i dont *care* if its better for readability, you do not need return spam in godot
@@ -49,6 +49,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# TODO: this script, while it works, sucks bad and needs to be rewritten
+	# BUG: if you change the camera, the player still moves along the original axises which is BAD and we HATES it
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -59,6 +60,11 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	update_sprite(direction)
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("temp_cam_left"):
+		$Pivot.rotation -= Vector3(0,45,0)
+	if Input.is_action_just_pressed("temp_cam_right"):
+		$Pivot.rotation += Vector3(0,45,0)
 
 func _on_sprite_animation_finished() -> void:
 	$Sprite.stop()
