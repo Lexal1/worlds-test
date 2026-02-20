729a95ef2f24316f3588d70c14507752e3485912
Lexal1 <86933786+Lexal1@users.noreply.github.com>
2026-02-19 17:08:02 -0700

    fall prevention
    fall prevention, comments, tiny little things that bug me I HATE NOT SNAPPING TO WHOLE NUMBERS

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -6,9 +6,9 @@ const JUMP_VELOCITY = 4.5
 func update_sprite(direction: Vector3) -> void:
 	#if direction.x != 0 and direction.y != 0 and direction.z != 0:
 	#print(direction)
-	#TODO: MAKE NOT SUCK!!!! This code will not work properly if the player rotation is different at all!
-	#      look into using an AnimationTree?
-	#      also looks horrific if you turn the camera because the sprite does not change
+	# TODO: MAKE NOT SUCK!!!! This code will not work properly if the player rotation is different at all!
+	#       look into using an AnimationTree?
+	#       also looks horrific if you turn the camera because the sprite does not change
 	if direction.x > 0 and direction.z > 0:
 		$Sprite.play("walk_se")
 		return # i dont *care* if its better for readability, you do not need return spam in godot
@@ -36,18 +36,18 @@ func update_sprite(direction: Vector3) -> void:
 		return
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	## Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if Input.is_action_pressed("button_2"):
 		SPEED = 6.0
 	else:
 		SPEED = 2.0
-	# Handle jump.
+	## Handle jump.
 	if Input.is_action_just_pressed("button_1") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	## Get the input direction and handle the movement/deceleration.
 	# TODO: this script, while it works, sucks bad and needs to be rewritten
 	# BUG: if you change the camera, the player still moves along the original axises which is BAD and we HATES it
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
@@ -65,6 +65,9 @@ func _physics_process(delta: float) -> void:
 		$Pivot.rotation -= Vector3(0,45,0)
 	if Input.is_action_just_pressed("temp_cam_right"):
 		$Pivot.rotation += Vector3(0,45,0)
+	
+	if position.y <= -10: ## dont fall out of the world dumbass
+		get_tree().reload_current_scene() 
 
 func _on_sprite_animation_finished() -> void:
 	$Sprite.stop()
